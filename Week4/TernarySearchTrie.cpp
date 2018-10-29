886d31b4ca57213ba87c84683d51690f0773b724
mviseu <mariaviseu@gmail.com>
2018-10-29 08:01:52 +0000

    Refactor Find and Insert methods in TernarySearchTrie

--- Week4/TernarySearchTrie.cpp
+++ Week4/TernarySearchTrie.cpp
@@ -7,30 +7,30 @@ auto Size(const std::string& str) -> int {
 	return static_cast<int>(str.size());
 }
 
+template <typename Val>
+auto SetValue(std::unique_ptr<Node<Val>> node, const Val& val) -> std::pair<bool, std::unique_ptr<Node<Val>>> {
+	if(node->val) {
+		return std::make_pair(false, std::move(node));
+	} else {
+		node->val = val;
+		return std::make_pair(true, std::move(node));
+	}
+}
+
 template<typename Val>
 auto InsertRec(std::unique_ptr<Node<Val>> node, const std::string& key, const Val& val, int depth) -> std::pair<bool, std::unique_ptr<Node<Val>>> {
 	if(!node) {
 		node = std::make_unique<Node<Val>>(key[depth]);
 	}
-	if(depth + 1 == Size(key)) {
-		if(key[depth] == node->charac) {
-			if(node->val) {
-				return std::make_pair(false, std::move(node));
-			} else {
-				node->val = val;
-				return std::make_pair(true, std::move(node));
-			}
-		}
-	}
 	auto inserted = false;
-	if(key[depth] == node->charac) {
+	if(key[depth] < node->charac) {
+		std::tie(inserted, node->left) = InsertRec(std::move(node->left), key, val, depth);
+	} else if(key[depth] > node->charac) {
+		std::tie(inserted, node->right) = InsertRec(std::move(node->right), key, val, depth);
+	} else if(depth + 1 < Size(key)) {
 		std::tie(inserted, node->mid) = InsertRec(std::move(node->mid), key, val, depth + 1);
 	} else {
-		if(key[depth] < node->charac) {
-			std::tie(inserted, node->left) = InsertRec(std::move(node->left), key, val, depth);
-		} else {
-			std::tie(inserted, node->right) = InsertRec(std::move(node->right), key, val, depth);
-		}
+		return SetValue(std::move(node), val);	
 	}
 	return std::make_pair(inserted, std::move(node));
 } 
@@ -39,19 +39,14 @@ template<typename Val>
 auto FindRec(const std::unique_ptr<Node<Val>>& node, const std::string& key, int depth) -> std::optional<Val> {
 	if(!node) {
 		return std::nullopt;
-	}
-	if(depth + 1 == Size(key)) {
-		if(key[depth] == node->charac)  {
-			return node->val;
-		}
-	}
-	if(key[depth] == node->charac)  {
-		return FindRec(node->mid, key, depth + 1);
-	}
-	if(key[depth] < node->charac) {
+	} else if(key[depth] < node->charac) {
 		return FindRec(node->left, key, depth);
-	} else {
+	} else if(key[depth] > node->charac) {
 		return FindRec(node->right, key, depth);
+	} else if(depth + 1 < Size(key)) {
+		return FindRec(node->mid, key, depth + 1);
+	} else {
+		return node->val;
 	}
 } 
 
